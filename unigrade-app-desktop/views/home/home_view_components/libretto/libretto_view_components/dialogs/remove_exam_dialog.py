fd9d03e035c995e4e176a3ac0fc9aef036f7acdf
Umberto Della Monica <umberto.dellamonica@hotmail.it>
2025-09-27 18:01:25 +0200

    update and fix button

--- unigrade-app-desktop/views/home/home_view_components/libretto/libretto_view_components/dialogs/remove_exam_dialog.py
+++ unigrade-app-desktop/views/home/home_view_components/libretto/libretto_view_components/dialogs/remove_exam_dialog.py
@@ -9,16 +9,23 @@ class RemoveExamDialog(ctk.CTkToplevel):
         self.geometry("300x150")
         self.grab_set()
 
+        # Label
         label = ctk.CTkLabel(self, text=f"Vuoi rimuovere l'esame '{values[0]}'?")
         label.pack(pady=20)
 
-        confirm_btn = ctk.CTkButton(
-            self, text="Conferma", fg_color="red", command=self._on_confirm
-        )
-        confirm_btn.pack(side="left", padx=30, pady=10)
+        # Frame per i pulsanti
+        button_frame = ctk.CTkFrame(self, fg_color="transparent")
+        button_frame.pack(pady=10, fill="x")
 
-        cancel_btn = ctk.CTkButton(self, text="Annulla", command=self.destroy)
-        cancel_btn.pack(side="right", padx=30, pady=10)
+        # Pulsante Conferma
+        confirm_btn = ctk.CTkButton(
+            button_frame, text="Conferma", fg_color="red", command=self._on_confirm
+        )
+        confirm_btn.pack(side="left", expand=True, padx=20)
+
+        # Pulsante Annulla
+        cancel_btn = ctk.CTkButton(button_frame, text="Annulla", command=self.destroy)
+        cancel_btn.pack(side="right", expand=True, padx=20)
 
     def _on_confirm(self):
         self.confirmed = True
